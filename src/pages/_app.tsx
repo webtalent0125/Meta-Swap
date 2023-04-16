d9ebaa1161e7e534ce2bf46d135ab37f5aaf2f41
webtalent0125 <talenteddev0125@gmail.com>
2023-04-16 14:12:09 +0900

    fix: test tas

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,8 @@
 import '../styles/global.css';
 
 import type { AppProps } from 'next/app';
-import { useEffect } from 'react';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  useEffect(() => {
-    const use = async () => {
-      /* eslint-disable-next-line */
-      (await import('tw-elements')).default;
-    };
-    use();
-  }, []);
   return <Component {...pageProps} />;
 };
 
