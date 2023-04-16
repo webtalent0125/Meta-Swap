5449425d26920f77ab4d65a32b95796b880a2309
webtalent0125 <talenteddev0125@gmail.com>
2023-04-16 14:50:23 +0900

    fix: responsive

--- src/templates/Main.tsx
+++ src/templates/Main.tsx
@@ -6,14 +6,14 @@ type IMainProps = {
 };
 
 const Main = (props: IMainProps) => (
-  <>
+  <div className="flex justify-center">
     {props.meta}
     <main className="container text-title">
       <div className="flex h-screen w-[100%] items-center justify-center">
         {props.children}
       </div>
     </main>
-  </>
+  </div>
 );
 
 export { Main };
